7e56d48ffe9b50a5a19089a8b5da5e47d51f3af1
edarlinae <edarlinae@gmail.com>
2025-09-20 19:12:26 +0200

    cabecera responsive

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,4 +8,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrls: ['./header.component.scss'],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  logoUrl = '/assets/logoKairos.png';
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Cerrar menú al cambiar tamaño a escritorio
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth >= 980 && this.menuOpen) this.menuOpen = false;
+  }
+
+  // Cerrar menú al navegar (opcional si usas routerLink)
+  closeMenu() { this.menuOpen = false; }
+}
