da008e51282d7b8ffbbf76441e33d11406f3508c
edarlinae <edarlinae@gmail.com>
2025-09-21 11:31:27 +0200

    remate apartado equipo

--- src/app/pages/equipo/equipo.component.ts
+++ src/app/pages/equipo/equipo.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-equipo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './equipo.component.html',
-  styleUrl: './equipo.component.scss'
+  styleUrls: ['./equipo.component.scss'],
 })
-export class EquipoComponent {
-
-}
+export class EquipoComponent {}
