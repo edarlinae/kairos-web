1451650fde2bed0d82d2152db69f2b905a40e32d
edarlinae <edarlinae@gmail.com>
2025-09-21 11:50:33 +0200

    Edicion contacto final

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss'],
 })
